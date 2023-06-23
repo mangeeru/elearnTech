3ba669232fa14f9a3b0e2cf0f315df84c76cdf03
mange <mange@Mangeeru>
2023-06-23 15:21:49 +0530

    first commit

---
+++ src/main/java/com/immortal/sol/api/ElearnTechApplication.java
@@ -0,0 +1,13 @@
+package com.immortal.sol.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElearnTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElearnTechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/immortal/sol/api/ElearnTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.immortal.sol.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElearnTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
